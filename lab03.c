63c6ba1e6e399abf2a317a3f18ba097865ef8db3
thamycoelho <thamycoelho10@gmail.com>
2017-05-27 17:07:05 -0300

    Update lab03.c

--- lab03.c
+++ lab03.c
@@ -13,40 +13,39 @@ typedef struct numero{
 	numerao *cabeca;
 } numero;
 
-void insereLista(int aux, numerao **number);
+void insereLista(int aux, numerao *number);
 numerao *criaNumeroString(char *str, int tam);
 void imprimeLista (numerao *num);
-
-
 
 int main(int argc, char **argv)
 {
 	char str[101];
 	int tam, x;
-	numerao *n1;
+	numerao *n1, *n2;
 	
-	scanf("%s", str);
-	/*
-	while(strcmp(str, "FIM") != 0){
+	str[0] = '0'; //para entrar de primeira no while
+	
+	//while(strcmp(str, "FIM") != 0){
 		scanf("%s", str);
+		tam = strlen(str);
+		n1 = criaNumeroString(str, tam);
+		scanf("%s", str);
+		tam = strlen(str);
+		n2 = criaNumeroString(str, tam);
 		
-	}
-	*/
-	
-	tam = strlen(str);
-	printf("%d\n", tam);
-	
-	n1 = criaNumeroString(str, tam);
+	//}
 	
 	imprimeLista (n1);
+	printf("\n");
+	imprimeLista (n2);
 	
 
 	return 0;
 }
 
-numero *criaNumeroString(char *str, int tam){
-	numerao *number;
+numerao *criaNumeroString(char *str, int tam){
 	numero *num;
+	numerao *number;
 	
 	num = malloc(sizeof(numero));
 	int pos=0, i, aux;
@@ -55,45 +54,36 @@ numero *criaNumeroString(char *str, int tam){
 		num->sinal = 1;
 		pos = 1;
 	}
-	
 	number = malloc(sizeof(numerao));
+	num->cabeca = number;
 	
-	if(!number){
-		printf("NAO ALOCOU MEMORIA");
-	}
-	number->prox = NULL;
 	
-	printf("\n");
 	for(i=tam-1; i >= pos; i--){
 		aux = atoi(&str[i]);
-		printf("%d ", aux);
-		insereLista(aux, &number);
+		insereLista(aux, number);
 		str[i] = '\0';
 	}
-		printf("\n");
 
-	num->cabeca = number;
-	return num;
+	return number;
 }
 	
-
-void insereLista(int aux, numero **number){
-	numerao *novo, *p = (*number->cabeca);
-	novo = (numerao *)malloc(sizeof(numerao));
-    novo->dado = aux;
-    novo->prox = NULL;
-    
-    while(p->prox != NULL){
-		p = p->prox;
-	}
-    p->prox = novo;   
-}
+void insereLista(int aux, numerao *number){
+	numerao *novo, *p= number;
 	
-void imprimeLista (numero *num) {
-   numerao *p;
-   
-   for (p = num->cabeca; p != NULL; p = p->prox) 
-      printf ("%d\n", p->dado);
+	novo =(numerao *) malloc(sizeof(numerao));
+	
+	novo->dado = aux;
+	novo->prox = NULL;
+	
+		
+	while(p->prox != NULL)
+			p = p->prox;
+		
+		p->prox = novo;
 }
 
-
+void imprimeLista (numerao *num) {
+	numerao *p;
+	for (p = num->prox; p != NULL; p = p->prox) 
+      printf ("%d", p->dado);
+}
